da0072772d64eb89560057ca4d63b7de25439945
Sockthing <sockthinggaming@gmail.com>
2021-03-30 00:59:06 +0200

    Made changes to fix problems
    Yes, yes. Much code.
    Thanks dawn!

--- src/minecraft/net/minecraft/src/MapAddonDefs.java
+++ src/minecraft/net/minecraft/src/MapAddonDefs.java
@@ -16,70 +16,69 @@ public class MapAddonDefs {
 		
 		//define colors here
 		
-		MapColor[] mapColorArray = new MapColor[14];
 		if (colorPalette == 1) //sepia colors
 	    {
-			MapColor grassColor = new MapColor(1, 11506271);
-			MapColor sandColor = new MapColor(2, 16703931);
-			MapColor clothColor = new MapColor(3, 12755067);
-			MapColor tntColor = new MapColor(4, 8076568);
-			MapColor iceColor = new MapColor(5, 13543821);
-			MapColor ironColor = new MapColor(6, 12755067);
-			MapColor foliageColor = new MapColor(7, 6178837);
-		    MapColor snowColor = new MapColor(8, 16777215);
-		    MapColor clayColor = new MapColor(9, 12360567);
-		    MapColor dirtColor = new MapColor(10, 10318406);
-		    MapColor stoneColor = new MapColor(11, 9073242);
-		    MapColor waterColor = new MapColor(12, 7228218);
-		    MapColor woodColor = new MapColor(13, 7556902);
+			MapColor.grassColor = new MapColor(1, 11506271);
+			MapColor.sandColor = new MapColor(2, 16703931);
+			MapColor.clothColor = new MapColor(3, 12755067);
+			MapColor.tntColor = new MapColor(4, 8076568);
+			MapColor.iceColor = new MapColor(5, 13543821);
+			MapColor.ironColor = new MapColor(6, 12755067);
+			MapColor.foliageColor = new MapColor(7, 6178837);
+		    MapColor.snowColor = new MapColor(8, 16777215);
+		    MapColor.clayColor = new MapColor(9, 12360567);
+		    MapColor.dirtColor = new MapColor(10, 10318406);
+		    MapColor.stoneColor = new MapColor(11, 9073242);
+		    MapColor.waterColor = new MapColor(12, 7228218);
+		    MapColor.woodColor = new MapColor(13, 7556902);
 	    }
-		if (colorPalette == 2) //desaturated colors
+		else if (colorPalette == 2) //desaturated colors
 	    {
-			MapColor grassColor = new MapColor(1, 9607307);
-			MapColor sandColor = new MapColor(2, 15197918);
-			MapColor clothColor = new MapColor(3, 10987431);
-			MapColor tntColor = new MapColor(4, 6243396);
-			MapColor iceColor = new MapColor(5, 11053235);
-			MapColor ironColor = new MapColor(6, 10987431);
-			MapColor foliageColor = new MapColor(7, 4280129);
-		    MapColor snowColor = new MapColor(8, 16777215);
-		    MapColor clayColor = new MapColor(9, 10987690);
-		    MapColor dirtColor = new MapColor(10, 8484978);
-		    MapColor stoneColor = new MapColor(11, 7368816);
-		    MapColor waterColor = new MapColor(12, 5395047);
-		    MapColor woodColor = new MapColor(13, 5723217);
+			MapColor.grassColor = new MapColor(1, 9607307);
+			MapColor.sandColor = new MapColor(2, 15197918);
+			MapColor.clothColor = new MapColor(3, 10987431);
+			MapColor.tntColor = new MapColor(4, 6243396);
+			MapColor.iceColor = new MapColor(5, 11053235);
+			MapColor.ironColor = new MapColor(6, 10987431);
+			MapColor.foliageColor = new MapColor(7, 4280129);
+		    MapColor.snowColor = new MapColor(8, 16777215);
+		    MapColor.clayColor = new MapColor(9, 10987690);
+		    MapColor.dirtColor = new MapColor(10, 8484978);
+		    MapColor.stoneColor = new MapColor(11, 7368816);
+		    MapColor.waterColor = new MapColor(12, 5395047);
+		    MapColor.woodColor = new MapColor(13, 5723217);
 	    }
-		if (colorPalette == 3) //happy colors
+		else if (colorPalette == 3) //happy colors
 	    {
-			MapColor grassColor = new MapColor(1, 9229842);
-			MapColor sandColor = new MapColor(2, 16771525);
-			MapColor clothColor = new MapColor(3, 10987431);
-			MapColor tntColor = new MapColor(4, 12461619);
-			MapColor iceColor = new MapColor(5, 10526975);
-			MapColor ironColor = new MapColor(6, 10987431);
-			MapColor foliageColor = new MapColor(7, 4491546);
-		    MapColor snowColor = new MapColor(8, 16777215);
-		    MapColor clayColor = new MapColor(9, 11512505);
-		    MapColor dirtColor = new MapColor(10, 14450447);
-		    MapColor stoneColor = new MapColor(11, 8092539);
-		    MapColor waterColor = new MapColor(12, 1997285);
-		    MapColor woodColor = new MapColor(13, 7954203);
+			MapColor.grassColor = new MapColor(1, 9229842);
+			MapColor.sandColor = new MapColor(2, 16771525);
+			MapColor.clothColor = new MapColor(3, 10987431);
+			MapColor.tntColor = new MapColor(4, 12461619);
+			MapColor.iceColor = new MapColor(5, 10526975);
+			MapColor.ironColor = new MapColor(6, 10987431);
+			MapColor.foliageColor = new MapColor(7, 4491546);
+		    MapColor.snowColor = new MapColor(8, 16777215);
+		    MapColor.clayColor = new MapColor(9, 11512505);
+		    MapColor.dirtColor = new MapColor(10, 14450447);
+		    MapColor.stoneColor = new MapColor(11, 8092539);
+		    MapColor.waterColor = new MapColor(12, 1997285);
+		    MapColor.woodColor = new MapColor(13, 7954203);
 	    }
 		else //default colors
 		{
-			MapColor grassColor = new MapColor(1, 8368696);
-			MapColor sandColor = new MapColor(2, 16247203);
-			MapColor clothColor = new MapColor(3, 10987431);
-			MapColor tntColor = new MapColor(4, 16711680);
-			MapColor iceColor = new MapColor(5, 10526975);
-			MapColor ironColor = new MapColor(6, 10987431);
-			MapColor foliageColor = new MapColor(7, 31744);
-		    MapColor snowColor = new MapColor(8, 16777215);
-		    MapColor clayColor = new MapColor(9, 10791096);
-		    MapColor dirtColor = new MapColor(10, 12020271);
-		    MapColor stoneColor = new MapColor(11, 7368816);
-		    MapColor waterColor = new MapColor(12, 4210943);
-		    MapColor woodColor = new MapColor(13, 6837042);
+			MapColor.grassColor = new MapColor(1, 8368696);
+			MapColor.sandColor = new MapColor(2, 16247203);
+			MapColor.clothColor = new MapColor(3, 10987431);
+			MapColor.tntColor = new MapColor(4, 16711680);
+			MapColor.iceColor = new MapColor(5, 10526975);
+			MapColor.ironColor = new MapColor(6, 10987431);
+			MapColor.foliageColor = new MapColor(7, 31744);
+		    MapColor.snowColor = new MapColor(8, 16777215);
+		    MapColor.clayColor = new MapColor(9, 10791096);
+		    MapColor.dirtColor = new MapColor(10, 12020271);
+		    MapColor.stoneColor = new MapColor(11, 7368816);
+		    MapColor.waterColor = new MapColor(12, 4210943);
+		    MapColor.woodColor = new MapColor(13, 6837042);
 	    }
 	}
 	    
--- src/minecraft/net/minecraft/src/MapColor.java
+++ src/minecraft/net/minecraft/src/MapColor.java
@@ -9,49 +9,50 @@ public class MapColor
     /**
      * Holds all the 16 colors used on maps, very similar of a pallete system.
      */
-    public static final MapColor[] mapColorArray = new MapColor[16];
+ 
+    public static MapColor[] mapColorArray = new MapColor[16];
 
     /** The map color for Air blocks */
-    public static final MapColor airColor = new MapColor(0, 0);
+    public static  MapColor airColor = new MapColor(0, 0);
 
     /** this is the grass color in html format */
-    public static final MapColor grassColor = new MapColor(1, 8368696);
+    public static MapColor grassColor = new MapColor(1, 8368696);
 
     /** This is the color of the sand */
-    public static final MapColor sandColor = new MapColor(2, 16247203);
+    public static MapColor sandColor = new MapColor(2, 16247203);
 
     /** The map color for Cloth and Sponge blocks */
-    public static final MapColor clothColor = new MapColor(3, 10987431);
+    public static MapColor clothColor = new MapColor(3, 10987431);
 
     /** The map color for TNT blocks */
-    public static final MapColor tntColor = new MapColor(4, 16711680);
+    public static MapColor tntColor = new MapColor(4, 16711680);
 
     /** The map color for Ice blocks */
-    public static final MapColor iceColor = new MapColor(5, 10526975);
+    public static MapColor iceColor = new MapColor(5, 10526975);
 
     /** The map color for Iron blocks */
-    public static final MapColor ironColor = new MapColor(6, 10987431);
+    public static MapColor ironColor = new MapColor(6, 10987431);
 
     /** The map color for Leaf, Plant, Cactus, and Pumpkin blocks. */
-    public static final MapColor foliageColor = new MapColor(7, 31744);
+    public static MapColor foliageColor = new MapColor(7, 31744);
 
     /** The map color for Snow Cover and Snow blocks */
-    public static final MapColor snowColor = new MapColor(8, 16777215);
+    public static MapColor snowColor = new MapColor(8, 16777215);
 
     /** The map color for Clay blocks */
-    public static final MapColor clayColor = new MapColor(9, 10791096);
+    public static MapColor clayColor = new MapColor(9, 10791096);
 
     /** The map color for Dirt blocks */
-    public static final MapColor dirtColor = new MapColor(10, 12020271);
+    public static MapColor dirtColor = new MapColor(10, 12020271);
 
     /** The map color for Stone blocks */
-    public static final MapColor stoneColor = new MapColor(11, 7368816);
+    public static MapColor stoneColor = new MapColor(11, 7368816);
 
     /** The map color for Water blocks */
-    public static final MapColor waterColor = new MapColor(12, 4210943);
+    public static MapColor waterColor = new MapColor(12, 4210943);
 
     /** The map color for Wood blocks */
-    public static final MapColor woodColor = new MapColor(13, 6837042);
+    public static MapColor woodColor = new MapColor(13, 6837042);
 
     /** Holds the color in RGB value that will be rendered on maps. */
     public final int colorValue;
@@ -65,4 +66,5 @@ public class MapColor
         this.colorValue = par2;
         mapColorArray[par1] = this;
     }
+    
 }
